7d829a01ec0954c82fe3eca2e9009092e4e5d7db
subendhu19 <subendhu.iitm@gmail.com>
2019-09-09 10:12:01 -0400

    Temp fix - use first 100 tokens from articles for pretrained bert

--- src/scripts/bert_trainer.py
+++ src/scripts/bert_trainer.py
@@ -60,7 +60,7 @@ class EIDatasetReader(DatasetReader):
         fields = {
             # 'article': ListField([TextField([Token(x) for x in chunk], self.token_indexers)
             #                       for chunk in article_chunks]),
-            'article': TextField([Token(x) for x in article_text[:200]], self.token_indexers),
+            'article': TextField([Token(x) for x in article_text[:100]], self.token_indexers),
             'outcome': TextField([Token(x) for x in outcome], self.token_indexers),
             'intervention': TextField([Token(x) for x in intervention], self.token_indexers),
             'comparator': TextField([Token(x) for x in comparator], self.token_indexers),
