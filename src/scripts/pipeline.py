abac2fd8dafbea9b5f559a2b0eeb65f35d089910
subendhu19 <subendhu.iitm@gmail.com>
2019-10-18 14:55:18 -0400

    Minor fix

--- src/scripts/pipeline.py
+++ src/scripts/pipeline.py
@@ -64,97 +64,98 @@ class PipelineDatasetReader(DatasetReader):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Evidence sentence classifier')
-    parser.add_argument('--k', type=int, default=3,
-                        help='number of evidence sentences to pick from the classifier (default: 3)')
-    args = parser.parse_args()
+    with torch.no_grad():
+        parser = argparse.ArgumentParser(description='Evidence sentence classifier')
+        parser.add_argument('--k', type=int, default=3,
+                            help='number of evidence sentences to pick from the classifier (default: 3)')
+        args = parser.parse_args()
 
-    bert_token_indexer = {'bert': PretrainedBertIndexer('scibert/vocab.txt', max_pieces=512)}
+        bert_token_indexer = {'bert': PretrainedBertIndexer('scibert/vocab.txt', max_pieces=512)}
 
-    pipeline_train = pickle.load(open('data/train_instances.p', 'rb'))
-    pipeline_val = pickle.load(open('data/val_instances.p', 'rb'))
-    pipeline_test = pickle.load(open('data/test_instances.p', 'rb'))
+        # pipeline_train = pickle.load(open('data/train_instances.p', 'rb'))
+        pipeline_val = pickle.load(open('data/val_instances.p', 'rb'))
+        pipeline_test = pickle.load(open('data/test_instances.p', 'rb'))
 
-    pipeline_reader = PipelineDatasetReader(bert_token_indexer)
-    p_train = pipeline_reader.read(pipeline_train)
-    p_val = pipeline_reader.read(pipeline_val)
-    p_test = pipeline_reader.read(pipeline_test)
+        pipeline_reader = PipelineDatasetReader(bert_token_indexer)
+        # p_train = pipeline_reader.read(pipeline_train)
+        p_val = pipeline_reader.read(pipeline_val)
+        p_test = pipeline_reader.read(pipeline_test)
 
-    p_vocab = Vocabulary.from_instances(p_train + p_val + p_test)
+        p_vocab = Vocabulary.from_instances(p_val + p_test)
 
-    bert_token_embedding = PretrainedBertEmbedder(
-        'scibert/weights.tar.gz', requires_grad=False
-    )
+        bert_token_embedding = PretrainedBertEmbedder(
+            'scibert/weights.tar.gz', requires_grad=False
+        )
 
-    word_embeddings = BasicTextFieldEmbedder(
-        {"bert": bert_token_embedding},
-        {"bert": ['bert']},
-        allow_unmatched_keys=True
-    )
+        word_embeddings = BasicTextFieldEmbedder(
+            {"bert": bert_token_embedding},
+            {"bert": ['bert']},
+            allow_unmatched_keys=True
+        )
 
-    ev_classifier = Classifier(word_embeddings=word_embeddings,
-                               vocab=p_vocab,
-                               loss='bce',
-                               hinge_margin=0)
-    predictor = Oracle(word_embeddings=word_embeddings,
-                       vocab=p_vocab)
+        ev_classifier = Classifier(word_embeddings=word_embeddings,
+                                   vocab=p_vocab,
+                                   loss='bce',
+                                   hinge_margin=0)
+        predictor = Oracle(word_embeddings=word_embeddings,
+                           vocab=p_vocab)
 
-    cuda_device = 0
+        cuda_device = 0
 
-    if torch.cuda.is_available():
-        ev_classifier = ev_classifier.cuda()
-        predictor = predictor.cuda()
-    else:
-        cuda_device = -1
+        if torch.cuda.is_available():
+            ev_classifier = ev_classifier.cuda()
+            predictor = predictor.cuda()
+        else:
+            cuda_device = -1
 
-    ev_classifier.load_state_dict(torch.load('model_checkpoints/f_evidence_sentence_classifier/best.th'))
-    predictor.load_state_dict(torch.load('model_checkpoints/f_oracle_full/best.th'))
+        ev_classifier.load_state_dict(torch.load('model_checkpoints/f_evidence_sentence_classifier/best.th'))
+        predictor.load_state_dict(torch.load('model_checkpoints/f_oracle_full/best.th'))
 
-    print('Classifier and Predictor models loaded successfully')
-    ev_classifier.eval()
-    predictor.eval()
+        print('Classifier and Predictor models loaded successfully')
+        ev_classifier.eval()
+        predictor.eval()
 
-    iterator = BasicIterator(batch_size=100)
-    iterator.index_with(p_vocab)
+        iterator = BasicIterator(batch_size=32)
+        iterator.index_with(p_vocab)
 
-    iterator_obj = iterator(p_test, num_epochs=1, shuffle=False)
-    generator_tqdm = Tqdm.tqdm(iterator_obj, total=iterator.get_num_batches(p_test))
+        iterator_obj = iterator(p_test, num_epochs=1, shuffle=False)
+        generator_tqdm = Tqdm.tqdm(iterator_obj, total=iterator.get_num_batches(p_test))
 
-    output_probs = []
-    for batch in generator_tqdm:
-        batch = nn_util.move_to_device(batch, cuda_device)
-        probs = ev_classifier.predict_evidence_probs(**batch)
-        for prob in probs:
-            output_probs.append(prob)
+        output_probs = []
+        for batch in generator_tqdm:
+            batch = nn_util.move_to_device(batch, cuda_device)
+            probs = ev_classifier.predict_evidence_probs(**batch)
+            for prob in probs:
+                output_probs.append(prob)
 
-    print('Obtained all sentence evidence probabilities - total {}'.format(len(output_probs)))
+        print('Obtained all sentence evidence probabilities - total {}'.format(len(output_probs)))
 
-    top_k_sentences = []
-    prob_counter = 0
-    for i in range(len(pipeline_test)):
-        sentences = [' '.join(s[0]) for s in pipeline_test[i]['sentence_span']]
-        probs = list(output_probs[prob_counter: prob_counter + len(sentences)])
-        prob_counter += len(sentences)
-        sorted_sentences = sorted(zip(sentences, probs), key=lambda x: x[1], reverse=True)
-        top_k = [s[0] for s in sorted_sentences[:args.k]]
-        top_k_sentences.append({'I': pipeline_test['I'],
-                                'C': pipeline_test['C'],
-                                'O': pipeline_test['O'],
-                                'y_label': pipeline_test['y_label'],
-                                'evidence': ' '.join(top_k)})
+        top_k_sentences = []
+        prob_counter = 0
+        for i in range(len(pipeline_test)):
+            sentences = [' '.join(s[0]) for s in pipeline_test[i]['sentence_span']]
+            probs = list(output_probs[prob_counter: prob_counter + len(sentences)])
+            prob_counter += len(sentences)
+            sorted_sentences = sorted(zip(sentences, probs), key=lambda x: x[1], reverse=True)
+            top_k = [s[0] for s in sorted_sentences[:args.k]]
+            top_k_sentences.append({'I': pipeline_test['I'],
+                                    'C': pipeline_test['C'],
+                                    'O': pipeline_test['O'],
+                                    'y_label': pipeline_test['y_label'],
+                                    'evidence': ' '.join(top_k)})
 
-    print('Obtained the top sentences from the evidence classifier')
+        print('Obtained the top sentences from the evidence classifier')
 
-    predictor_reader = EIDatasetReader(bert_token_indexer)
-    predictor_test = predictor_reader.read(top_k_sentences)
+        predictor_reader = EIDatasetReader(bert_token_indexer)
+        predictor_test = predictor_reader.read(top_k_sentences)
 
-    test_metrics = evaluate(predictor, predictor_test, iterator,
-                            cuda_device=cuda_device,
-                            batch_weight_key="")
+        test_metrics = evaluate(predictor, predictor_test, iterator,
+                                cuda_device=cuda_device,
+                                batch_weight_key="")
 
-    print('Test Data statistics:')
-    for key, value in test_metrics.items():
-        print(str(key) + ': ' + str(value))
+        print('Test Data statistics:')
+        for key, value in test_metrics.items():
+            print(str(key) + ': ' + str(value))
 
 
 if __name__ == '__main__':
